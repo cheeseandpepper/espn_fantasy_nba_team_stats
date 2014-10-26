fff1617e0334d8dba7681c86774764b44747815f
cheeseandpepper <cheeseandpepper@gmail.com>
2014-10-26 09:01:50 -0400

    updated and hopefully working

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(null, {file:"fantasy_team_stats.js"});
+  chrome.tabs.executeScript({file: "jquery-1.11.1.js"});
+  chrome.tabs.executeScript({file: "fantasy_team_stats.js"});
 });
